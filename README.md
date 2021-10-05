61c4b728e636d5e11fc5a2dc94cd765ab09eba44
nagehancapa <capanagehan@gmail.com>
2021-10-05 15:51:12 +0200

    initial commit, sequelize, connect to db

---
+++ README.md
@@ -0,0 +1,3 @@
+art gallery server
+
+A webapp to share artworks, like them and make bids for them. The server is made with Express, Sequelize ORM and JWT/Bcrypt Authentication.
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const loggerMiddleWare = require("morgan");
+const corsMiddleWare = require("cors");
+const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
+const bodyParserMiddleWare = express.json();
+const app = express();
+
+app.use(loggerMiddleWare("dev"));
+app.use(bodyParserMiddleWare);
+app.use(corsMiddleWare());
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
+app.use("/", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
