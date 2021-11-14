7ac21784d11f6943095c69cc454e82ce7ac6d7b2
nagehancapa <capanagehan@gmail.com>
2021-11-14 21:37:26 +0100

    changed url

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DB_URL,
     dialect: "postgres",
     operatorsAliases: "0",
   },
