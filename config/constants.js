df4f4d0f894f6d096122d458903598e9d196d44d
nagehancapa <capanagehan@gmail.com>
2021-11-15 12:04:22 +0100

    added host value to constants

--- config/constants.js
+++ config/constants.js
@@ -3,4 +3,5 @@ require("dotenv").config();
 module.exports = {
   SALT_ROUNDS: 10,
   PORT: process.env.PORT || 4000,
+  HOST: "0.0.0.0" || "localhost",
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const loggerMiddleWare = require("morgan");
 const cors = require("cors");
 const { PORT } = require("./config/constants");
+const { HOST } = require("./config/constants");
 const authRouter = require("./routers/auth");
 const artworkRouter = require("./routers/artworks");
 const bodyParserMiddleWare = express.json();
@@ -20,6 +21,6 @@ if (process.env.DELAY) {
 app.use("/", authRouter);
 app.use("/artworks", artworkRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Listening on port: ${PORT}`);
 });
